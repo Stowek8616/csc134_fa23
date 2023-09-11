9d4048050bb028ff48bf34c954db51d9035e0240
Stowek8616 <143423184+Stowek8616@users.noreply.github.com>
2023-09-11 11:38:05 -0400

    Finished Reciept Calculator

--- M2T1_Stowe/main.cpp
+++ M2T1_Stowe/main.cpp
@@ -4,33 +4,17 @@ using namespace std;
 
 int main()
 {
-    double menu_price = 0;
+    cout << "Recipt Calculator" << endl;
+    double menu_price = 5.99;
     double tax_percent = 0.07;
-    double item_price;
-    int num_items;
-    string item_name;
-
-    cout << "Welcome to the CSC 134 Grill." << endl;
-
-    cout << "What would you like today? "<< endl;
-    cin >> item_name;
-
-    cout << "What's the price on that item? " << endl;
-    cin >> item_price;
-
-    cout << "How many " << item_name << " do you want? " << endl;
-    cin >> num_items;
-
-    menu_price = num_items * item_price;
     double tax_dollars = menu_price * tax_percent;
     double total_price = menu_price + tax_dollars;
+
     cout << fixed << setprecision(2);
 
-    cout << "Order: " << num_items << " " << item_name << "(s)" << endl;
-    cout << "Menu Price: $" << menu_price << endl;
-    cout << "Tax is:     $" << tax_dollars << endl;
-    cout << "________________________" << endl;
-    cout << "Your Total: $" << total_price << endl;
     cout << "Thank you for Dining with us!" << endl;
+    cout << "Your meal is $" << menu_price << endl;
+    cout << "Your Tax is $" << tax_dollars << endl;
+    cout << "Your Total is $" << total_price << endl;
     return 0;
 }
