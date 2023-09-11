7b577e2671b96312e64568d0d886873cbff92850
Stowek8616 <143423184+Stowek8616@users.noreply.github.com>
2023-09-11 11:26:11 -0400

    Finished the Order

--- M2T1_Stowe/main.cpp
+++ M2T1_Stowe/main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "Receipt Calculator" << endl;
-    double menu_price = 5.99;
-    double tax_percent = 0.07 // cumberland county tax
-    double tax_dollars = menu_price * tax_percent ;
-    double total_price = menu_price +tax_dollars;
+    double menu_price = 0;
+    double tax_percent = 0.07;
+    double item_price;
+    int num_items;
+    string item_name;
 
-    cout << fixed << endl
+    cout << "Welcome to the CSC 134 Grill." << endl;
+
+    cout << "What would you like today? "<< endl;
+    cin >> item_name;
+
+    cout << "What's the price on that item? " << endl;
+    cin >> item_price;
+
+    cout << "How many " << item_name << " do you want? " << endl;
+    cin >> num_items;
+
+    menu_price = num_items * item_price;
+    double tax_dollars = menu_price * tax_percent;
+    double total_price = menu_price + tax_dollars;
+    cout << fixed << setprecision(2);
+
+    cout << "Order: " << num_items << " " << item_name << "(s)" << endl;
+    cout << "Menu Price: $" << menu_price << endl;
+    cout << "Tax is:     $" << tax_dollars << endl;
+    cout << "________________________" << endl;
+    cout << "Your Total: $" << total_price << endl;
+    cout << "Thank you for Dining with us!" << endl;
     return 0;
 }
