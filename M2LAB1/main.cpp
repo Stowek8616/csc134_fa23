a5b4c1589f10d52e6d0933f357f4b24b27f8b25e
Stowek8616 <143423184+Stowek8616@users.noreply.github.com>
2023-09-13 11:13:44 -0400

    Finished the Program

--- M2LAB1/main.cpp
+++ M2LAB1/main.cpp
@@ -16,12 +16,29 @@ int main()
 {
     const double COST_PER_CUBIC_FOOT = 0.23;
     const double CHARGE_PER_CUBIC_FOOT = 0.5;
-    double Length;
-    double Width;
-    double Height;
-    double Volume = Length * Width * Height;
-    double Cost;
-    double Charge;
-    double Profit = Charge - Cost;
+    double length, width, height;
+    double volume;
+    double cost, charge, profit;
+
+    // Get the Dimensions
+    cout << "Enter the Length, Width, and Height in feet. Press ENTER after each value inputted." << endl;
+    cin >> length >> width >> height;
+
+    // Calculate the Cost
+    volume = length * width * height;
+
+    //Find the Cost and Price for that volume
+    cost = COST_PER_CUBIC_FOOT * volume;
+    charge = CHARGE_PER_CUBIC_FOOT * volume;
+
+    //Find the profit
+    profit = charge - cost;
+
+    //Output the answer
+    cout << "The crate is: " << volume << " cubic ft." << endl;
+    cout << "The cost to manufacture is: $" << cost << endl;
+    cout << "The charge to customer is:  $" << charge << endl;
+    cout << "The profit is:              $" << profit << endl;
+
     return 0;
 }
